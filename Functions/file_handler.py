ad3b1ceb487423e57fd845e080f5d491dfe7e280
Viet Anh <v1et4nh@googlemail.com>
2021-11-14 01:49:38 +0100

    Added json to file_handler.py

--- Functions/file_handler.py
+++ Functions/file_handler.py
@@ -1,4 +1,5 @@
 import os
+import json
 import pickle
 
 
@@ -15,6 +16,25 @@ def load_pickle(str_file_path):
     if os.path.exists(str_file_path):
         with open(str_file_path, 'rb') as f:
             tmp_file = pickle.load(f)
+            return tmp_file
+    else:
+        dict_tmp = {'Error': 'File does not exist'}
+        return dict_tmp
+
+
+def save_json(save_obj, str_file_path):
+    try:
+        with open(str_file_path, 'w') as f:
+            json.dump(save_obj, f, indent=4)
+        return 'SUCCESS'
+    except:
+        return 'ERROR'
+
+
+def load_json(str_file_path):
+    if os.path.exists(str_file_path):
+        with open(str_file_path, 'r') as f:
+            tmp_file = json.load(f)
             return tmp_file
     else:
         dict_tmp = {'Error': 'File does not exist'}
