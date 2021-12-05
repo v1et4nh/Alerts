ce842329b445f103b5159b9783fd1e6b95fce1cc
Viet Anh <v1et4nh@googlemail.com>
2021-12-05 21:58:36 +0100

    Updated format

--- Crypto_Alert/agc_mint_alert.py
+++ Crypto_Alert/agc_mint_alert.py
@@ -4,8 +4,11 @@ from time import sleep
 from Functions.file_handler import save_pickle, load_pickle
 from Functions.telegrambot import telegram_bot_sendtext, etherscan_api_key, bot_chatID_private
 
-
+NAME        = 'Alpha Girl Club'
 PICKLE_FILE = '../Data/agc_last_counter.pickle'
+ADDRESS     = '0x8c5029957bf42c61d19a29075dc4e00b626e5022'   # Alpha Girl Club
+OPENSEA     = 'alphagirlclub'
+SLEEP       = 1
 
 
 def get_last_message():
@@ -13,16 +16,22 @@ def get_last_message():
     try:
         return dict_last_messages['counter']
     except:
-        return 10000
+        return -100000
 
 
-def getEtherScanData():
-    # address = '0x348FC118bcC65a92dC033A951aF153d14D945312'  # CloneX
-    address = '0x074C532B1659bC47065a6c4e784F8965971C3e7c'   # Rebelz
-    address = '0x8c5029957bf42c61d19a29075dc4e00b626e5022'   # Alpha Girl Club
+def getEtherScanData(address=ADDRESS):
     tmp_dict = {'address': address, 'key': etherscan_api_key}
-
     return tmp_dict
+
+
+def getData(url):
+    res = requests.get(url)
+    if res.status_code != 200:
+        res = requests.get(url, headers={"User-Agent": "Mozilla/5.0"})
+        if res.status_code != 200:
+            return 'RequestsError'
+    data = res.json()
+    return data
 
 
 def getMintedAmount(dict_data):
@@ -32,30 +41,26 @@ def getMintedAmount(dict_data):
     the first 8 characters -> 0xe777df20
     """
     url = 'https://api.etherscan.io/api?module=account&action=balance&address='+dict_data['address']+'&tag=latest&apikey='+dict_data['key']
-    res = requests.get(url)
-    if res.status_code != 200:
-        res = requests.get(url, headers={"User-Agent": "Mozilla/5.0"})
-        if res.status_code != 200:
-            return 'RequestsError'
-
-    data = res.json()
+    data = getData(url)
     mintedAmount = int(int(data['result'])/0.08/1000000000000000000)
 
     return mintedAmount
 
 
 def getCurrentMintPrice(dict_data):
-    url = 'https://api.etherscan.io/api?module=proxy&action=eth_call&to='+dict_data['address']+'&data=0x8d859f3e&apikey='+dict_data['key']
-    res = requests.get(url)
-    if res.status_code != 200:
-        res = requests.get(url, headers={"User-Agent": "Mozilla/5.0"})
-        if res.status_code != 200:
-            return 'RequestsError'
-
-    data = res.json()
-    currentPrice = float(str(int(data['result'], 16)).replace('0', ''))/1000
+    url = 'https://api.etherscan.io/api?module=proxy&action=eth_call&to='+dict_data['address']+'&data=0x33039c7c&apikey='+dict_data['key']
+    data = getData(url)
+    currentPrice = float(str(int(data['result'], 16)).replace('0', ''))/100
 
     return currentPrice
+
+
+def getMaxSupply(dict_data):
+    url = 'https://api.etherscan.io/api?module=proxy&action=eth_call&to='+dict_data['address']+'&data=0xa62ee636&apikey='+dict_data['key']
+    data = getData(url)
+    maxSupply = int(data['result'], 16)
+
+    return maxSupply
 
 
 def getETHprice():
@@ -85,36 +90,47 @@ def getETHprice():
     return peur_val, pusd_val, priceinfo
 
 
-def getNumberOfHolders():
-    url = "https://opensea.io/collection/rebelz"
-    resp = requests.get(url)
+def getOSstats(collection=OPENSEA):
+    url = "https://api.opensea.io/api/v1/collection/" + collection
+    data = getData(url)
+    stats = data['collection']['stats']
+
+    return stats
 
 
-def run_agc_mint_counter():
+def run_mint_counter():
     dict_data    = getEtherScanData()
     last_counter = get_last_message()
     mint_counter = getMintedAmount(dict_data)
     print(last_counter)
     print(mint_counter)
-    if mint_counter - last_counter > 0:
-        amount_left = 10000 - mint_counter
-        message = 'Alpha Girl Club amount minted: *' + str(mint_counter) + '*\n\nOnly *' + str(amount_left) + '* AGC NFTs left :OOO'
-        # price = getCurrentMintPrice(dict_data)
-        # eur, usd, _ = getETHprice()
-        # eur_price = int(eur * price)
-        # usd_price = int(usd * price)
-        # message += '\n\nCurrent Mint Price: *' + str(price) + ' ETH* (' + str(eur_price) + ' EUR | ' + str(usd_price) + ' USD)'
+    if mint_counter - last_counter > -10:
+        maxSupply = getMaxSupply(dict_data)
+        amount_left = maxSupply - mint_counter
+        stats = getOSstats()
+        message  = 'Minted: *' + str(mint_counter) + '* | Holders: *' + str(stats['num_owners']) + '*'
+        message += '\nLeft: *' + str(amount_left) + '*'
+        message += '\n\nFloor Price: *' + str(stats['floor_price']) + ' ETH*'
+        message += '\nVolume traded: *' + str(int(stats['total_volume'])) + ' ETH*'
+        price = getCurrentMintPrice(dict_data)
+        eur, usd, _ = getETHprice()
+        eur_price = int(eur * price)
+        usd_price = int(usd * price)
+        message += '\n\nCurrent Mint Price: *' + str(price) + ' ETH* (' + str(eur_price) + ' EUR | ' + str(usd_price) + ' USD)'
         telegram_bot_sendtext(message, bot_chatID='-1001655992736')
-        # telegram_bot_sendtext(message, bot_chatID=bot_chatID_private)
         dict_counter = {'counter': mint_counter}
         save_pickle(dict_counter, PICKLE_FILE)
 
 
-if __name__ == '__main__':
+def main(time_intervall=SLEEP):
     while True:
         try:
             print(time.strftime('%X %x %Z'))
-            run_agc_mint_counter()
-            sleep(1)
+            run_mint_counter()
+            sleep(time_intervall)
         except:
             print('Restart...')
+
+
+if __name__ == '__main__':
+    main()
