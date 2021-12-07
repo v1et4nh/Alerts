ff5f60c5038de9cf39254269ce199b1da626dda4
Viet Anh <v1et4nh@googlemail.com>
2021-12-07 01:46:36 +0100

    Added Clone X Floor Price Alert

---
+++ Crypto_Alert/clone_x_floor_alert.py
@@ -0,0 +1,83 @@
+import requests
+import time
+from time import sleep
+from Functions.telegrambot import telegram_bot_sendtext, etherscan_api_key, bot_chatID_private
+
+NAME        = 'Clone X'
+OPENSEA     = 'clonex-mintvial'
+SLEEP       = 5
+
+
+def getData(url):
+    res = requests.get(url)
+    if res.status_code != 200:
+        res = requests.get(url, headers={"User-Agent": "Mozilla/5.0"})
+        if res.status_code != 200:
+            return 'RequestsError'
+    data = res.json()
+    return data
+
+
+def getETHprice():
+    url_eur = "https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=eur%2Cbtc&include_market_cap=true&include_24hr_change=true"
+    url_usd = "https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd%2Cbtc&include_market_cap=true&include_24hr_change=true"
+    data_eur = requests.get(url_eur).json()
+    peur = round(data_eur["ethereum"]["eur"], 2)
+    peur = format(peur, ",")
+    peur_val = float(peur.replace(',', ''))
+    data = requests.get(url_usd).json()
+    pusd = round(data["ethereum"]["usd"], 2)
+    pusd = format(pusd, ",")
+    pusd_val = float(pusd.replace(',', ''))
+    pbtc = round(data["ethereum"]["btc"], 8)
+    pchange = round(data["ethereum"]["usd_24h_change"], 2)
+    market = round(data["ethereum"]["usd_market_cap"])
+    market = format(market, ",")
+
+    priceinfo = f"""<b><ins><a href='https://coingecko.com/en/coins/ethereum/'>Ethereum | $ETH</a> Price:</ins></b>
+<b>💰 EUR:</b> €{peur}
+<b>💰 USD:</b> ${pusd}
+<b>🗿 BTC:</b> ฿{pbtc}
+<b>📈 24h change:</b> {pchange}%
+<b>💎 Market Cap:</b> ${market}
+"""
+
+    return peur_val, pusd_val, priceinfo
+
+
+def getOSstats(collection=OPENSEA):
+    url = "https://api.opensea.io/api/v1/collection/" + collection
+    data = getData(url)
+    stats = data['collection']['stats']
+
+    return stats
+
+
+def run_os_stats():
+    stats = getOSstats()
+    floor_price = float(stats['floor_price'])
+    message  = NAME + ': ' + str(floor_price)
+    print(message)
+    if floor_price < 3:
+        eur, usd, _ = getETHprice()
+        eur_price   = int(eur * floor_price)
+        usd_price   = int(usd * floor_price)
+        message    += '\n\nFloor Price: *' + str(stats['floor_price']) + ' ETH* (*' + str(eur_price) + ' EUR* | *' + str(usd_price) + ' USD*)'
+        message    += '\nVolume traded: *' + str(int(stats['total_volume'])) + ' ETH*'
+        message    += '\nHolders: *' + str(stats['num_owners']) + '*'
+        message    += '\n\n-----\nIf you have any issues or feedback, feel free to [contact me](tg://user?id=383615621) :)'
+        telegram_bot_sendtext(message, bot_chatID=bot_chatID_private)
+
+
+def main(time_intervall=SLEEP):
+    while True:
+        try:
+            print(time.strftime('%X %x %Z'))
+            run_os_stats()
+            sleep(time_intervall)
+        except:
+            print('Restart...')
+
+
+if __name__ == '__main__':
+    main()
