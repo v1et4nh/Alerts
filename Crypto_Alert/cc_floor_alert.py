10776358c36b81b4113a278586dab392bb2f0777
Viet Anh <v1et4nh@googlemail.com>
2021-12-13 01:46:26 +0100

    Added some new scripts

---
+++ Crypto_Alert/cc_floor_alert.py
@@ -0,0 +1,93 @@
+import requests
+import time
+from time import sleep
+from Functions.file_handler import save_pickle, load_pickle
+from Functions.telegrambot import telegram_bot_sendtext, bot_chatID_private
+
+NAME         = 'Crypto Champions'
+OPENSEA      = 'cryptochampionsnft'
+SLEEP        = 3
+PRICE_ALARM  = 999  # ETH
+PICKLE_FILE  = '../Data/cc_last_floor.pickle'
+ORIGIN_PRICE = 0.25
+
+
+def get_last_message():
+    dict_last_messages = load_pickle(PICKLE_FILE)
+    try:
+        return dict_last_messages['floor']
+    except:
+        return -100000
+
+
+def getData(url):
+    res = requests.get(url)
+    if res.status_code != 200:
+        res = requests.get(url, headers={"User-Agent": "Mozilla/5.0"})
+        if res.status_code != 200:
+            return 'RequestsError'
+    data = res.json()
+    return data
+
+
+def getETHprice():
+    url_eur = "https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=eur%2Cbtc&include_market_cap=true&include_24hr_change=true"
+    url_usd = "https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd%2Cbtc&include_market_cap=true&include_24hr_change=true"
+    data_eur = requests.get(url_eur).json()
+    peur = round(data_eur["ethereum"]["eur"], 2)
+    peur = format(peur, ",")
+    peur_val = float(peur.replace(',', ''))
+    data = requests.get(url_usd).json()
+    pusd = round(data["ethereum"]["usd"], 2)
+    pusd = format(pusd, ",")
+    pusd_val = float(pusd.replace(',', ''))
+
+    return peur_val, pusd_val
+
+
+def getOSstats(collection=OPENSEA):
+    url = "https://api.opensea.io/api/v1/collection/" + collection
+    data = getData(url)
+    stats = data['collection']['stats']
+
+    return stats
+
+
+def run_os_stats():
+    stats = getOSstats()
+    last_floor = get_last_message()
+    floor_price = float(stats['floor_price'])
+    message  = NAME + ': ' + str(floor_price) + 'ETH '
+    print(message)
+    if floor_price < PRICE_ALARM and abs(floor_price - last_floor) > -0.01:
+        eur, usd, = getETHprice()
+        eur_price = int(eur * floor_price)
+        usd_price = int(usd * floor_price)
+        url       = 'https://opensea.io/collection/' + OPENSEA
+        # percent   = float(floor_price/ORIGIN_PRICE) - 1
+        # if percent > 0:
+        #     percent_sign = '+'
+        # else:
+        #     percent_sign = '-'
+        # message  += percent_sign + str(round(percent*100, 2)) + '%'
+        message  += '\n\nFloor Price: *' + str(stats['floor_price']) + ' ETH* (*' + str(eur_price) + ' EUR* | *' + str(usd_price) + ' USD*)'
+        message  += '\nVolume traded: *' + str(int(stats['total_volume'])) + ' ETH*'
+        message  += '\nHolders: *' + str(stats['num_owners']) + '*'
+        message  += '\n\nOpen in [Opensea](' + url + ')'
+        telegram_bot_sendtext(message, bot_chatID='-1001648482623', disable_web_page_preview=True)
+        dict_floor = {'floor': floor_price}
+        save_pickle(dict_floor, PICKLE_FILE)
+
+
+def main(time_intervall=SLEEP):
+    while True:
+        try:
+            print(time.strftime('%X %x %Z'))
+            run_os_stats()
+            sleep(time_intervall)
+        except:
+            print('Restart...')
+
+
+if __name__ == '__main__':
+    main()
