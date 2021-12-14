6bd055bd7a138a2a2a99de64acee9dbbad3d42e1
Viet Anh <v1et4nh@googlemail.com>
2021-12-15 00:48:59 +0100

    Updated CC Floor Alert

--- Crypto_Alert/cc_floor_alert.py
+++ Crypto_Alert/cc_floor_alert.py
@@ -64,16 +64,11 @@ def run_os_stats():
         eur_price = int(eur * floor_price)
         usd_price = int(usd * floor_price)
         url       = 'https://opensea.io/collection/' + OPENSEA
-        # percent   = float(floor_price/ORIGIN_PRICE) - 1
-        # if percent > 0:
-        #     percent_sign = '+'
-        # else:
-        #     percent_sign = '-'
-        # message  += percent_sign + str(round(percent*100, 2)) + '%'
-        message  += '\n\nFloor Price: *' + str(stats['floor_price']) + ' ETH* (*' + str(eur_price) + ' EUR* | *' + str(usd_price) + ' USD*)'
+        message  = 'Floor Price: *' + str(stats['floor_price']) + ' ETH* (*' + str(eur_price) + ' EUR* | *' + str(usd_price) + ' USD*)'
         message  += '\nVolume traded: *' + str(int(stats['total_volume'])) + ' ETH*'
         message  += '\nHolders: *' + str(stats['num_owners']) + '*'
-        message  += '\n\nOpen in [Opensea](' + url + ')'
+        message  += '\n\nView in [Opensea](' + url + ')'
+        message  += '\n\n-----\nIf you have any issues or feedback, feel free to [contact me](tg://user?id=383615621) :)'
         telegram_bot_sendtext(message, bot_chatID='-1001648482623', disable_web_page_preview=True)
         dict_floor = {'floor': floor_price}
         save_pickle(dict_floor, PICKLE_FILE)
