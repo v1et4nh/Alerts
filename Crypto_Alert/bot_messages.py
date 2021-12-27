ff2e6f7952bc1d0c7b26c1292b3da3634a775ef9
Viet Anh <v1et4nh@googlemail.com>
2021-12-27 23:13:19 +0100

    Fixed floorbot

---
+++ Crypto_Alert/bot_messages.py
@@ -0,0 +1,33 @@
+# General
+donate_message = """
+I am free of charge, but I do still have some running costs. 
+My developer will be very grateful if you support him by sending him a little donation:
+
+*_Ethereum-Address_*: 
+0x250103C32239Dad3F31D121d75Da22353C6FF429\n
+*_ENS-Address_*: 
+vietanh.eth\n
+*_Paypal_*: 
+https://paypal.me/v1et4nh
+"""
+
+
+# ------------------------------------------------------------------------
+# Floor Bot
+floorbot_welcome_message = """
+Hi! I am your personal Opensea Floor Alert Bot.\n
+I will send you a message if the floor price of your chosen collection changes or falls below your self-defined threshold.\n
+Which collection do you want to track?
+\nPlease send me the opensea-url \n(e.g. https://opensea.io/collection/clonex):
+"""
+
+floorbot_help_message = """
+/start - Set an opensea-url and start the alert
+/stop - Stop the alert
+/floor - Get the current floor price
+/collection - Show the current tracked collection
+/donate - Buy me a coffee or even better: a NFT ;)
+/contact - Contact me regarding the development of the bot
+/help - Show the command list of this bot
+"""
+
