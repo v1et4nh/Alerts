9f411937a99267ea942658fae9b217f81d13b2ed
Viet Anh <v1et4nh@googlemail.com>
2022-05-20 01:51:37 +0200

    Added new shamanzs_mint_alert.py

--- Playground/discord_v1_Test.py
+++ Playground/discord_v1_Test.py
@@ -10,7 +10,6 @@ from discord_ui import UI, SelectOption, SelectMenu
 from dotenv import load_dotenv
 from asyncio import TimeoutError
 from Functions.file_handler import load_pickle, save_pickle
-from Functions.telegrambot import etherscan_api_key
 from Functions.scraping_tools import getOSstats, get_name, get_coin, getOScollection, getETHprice
 
 load_dotenv()
@@ -19,22 +18,25 @@ GUILD   = os.getenv('DISCORD_GUILD_STS_V1')
 OS_API  = str(os.getenv('OPENSEA_API_KEY'))
 OS_API_1  = str(os.getenv('OPENSEA_API_KEY'))
 OS_API_2  = str(os.getenv('OPENSEA_API_KEY_2'))
-PICKLE_FILE_FLOOR      = '../Data/discord_last_floor_test.pickle'
-PICKLE_FILE_COLLECTION = '../Data/discord_collection.pickle'
-PICKLE_FILE_COIN       = '../Data/discord_coin.pickle'
-PICKLE_FILE_SNIPER     = '../Data/discord_sniper.pickle'
-PICKLE_FILE_DATA       = '../Data/discord_data.pickle'
-
+PICKLE_FILE_DATA = '../Data/discord_data.pickle'
+TIMEOUT = 60
 
 client = commands.Bot(" ")
 ui = UI(client)
 
 
+def check_wait_for(message):
+    return message.author
+
+
 def get_dict_data():
     dict_blueprint = {
-        'floor': {},
-        'sales': {},
-        'sniper': {}
+        'coin':   {},
+        'floor':  {},
+        'gas':    {},
+        'sales':  {},
+        'sniper': {},
+        'wallet': {}
     }
     dict_data = load_pickle(PICKLE_FILE_DATA)
     try:
@@ -43,16 +45,6 @@ def get_dict_data():
     except:
         dict_data = dict_blueprint
     return dict_data
-
-
-def get_last_floor(collection):
-    dict_floor = load_pickle(PICKLE_FILE_FLOOR)
-    try:
-        if collection not in dict_floor:
-            dict_floor[collection] = 0
-    except:
-        dict_floor[collection] = 0
-    return dict_floor
 
 
 def get_opensea_slug(message):
@@ -90,23 +82,68 @@ async def on_message(message: discord.Message):
             SelectOption("Wallet Tracker", label="Wallet", description="Track a wallet for buy/sell activities")
         ], max_values=1)])
         try:
-            sel = await msg.wait_for("select", client, by=message.author, timeout=120)
+            sel = await msg.wait_for("select", client, by=message.author, timeout=TIMEOUT)
             await sel.respond(f"You selected {sel.selected_values[0]}")
             selected = sel.selected_options[0].content
             if selected == "Breakeven":
                 pass
             elif selected == "Coin":
-                pass
+                msg = await message.channel.send("What coin do you want to track?\n"
+                                                 "Please find the coin on https://nomics.com and send me the url to the coin:")
+                resp = await client.wait_for("message", timeout=TIMEOUT)
+                url  = resp.content
+                slug = url.split('/')[-1].split('-')[0].upper()
+                name, currency, price = '', 0, 0
+                try:
+                    name, currency, price = get_coin(slug)
+                    bool_success = True
+                except:
+                    bool_success = False
+                if not bool_success:
+                    await message.channel.send("Something went wrong. Please try again!")
+                else:
+                    if name is None:
+                        await message.channel.send("Error: Unknown currency!")
+                    else:
+                        msg = await message.channel.send("Set the change rate:")
+                        resp = await client.wait_for("message", timeout=TIMEOUT)
+                        if '%' in resp.content:
+                            change_rate = float(resp.content.replace('%', '').strip()) / 100
+                            change_type = 'relative'
+                            change_text = f"{change_rate*100}% (relative)"
+                        else:
+                            change_rate = float(resp.content.strip())
+                            change_type = 'absolute'
+                            change_text = f"{change_rate} (absolute)"
+                        dict_data = get_dict_data()
+                        if currency not in dict_data['coin']:
+                            dict_data['coin'][currency] = {
+                                'url':      url,
+                                'name':     name,
+                                'currency': currency,
+                                'slug':     slug,
+                                'last_price': 0.0,
+                                'channel_id': {str(message.channel.id): [change_type, change_rate]},
+                            }
+                        else:
+                            dict_data['coin'][currency]['channel_id'][str(message.channel.id)] = [change_type, change_rate]
+                        save_pickle(dict_data, PICKLE_FILE_DATA)
+                        message_to_send = f"Success!\n" \
+                                          f"Tracker is set up for the coin\n" \
+                                          f">>>>> *{name}* <<<<<\n" \
+                                          f"with a change rate of {change_text}!\n"
+                        await message.channel.send(message_to_send)
+
             elif selected == "Floor":
                 msg  = await message.channel.send("Which collection floor do you want to track?\n"
                                                   "Please send me the opensea-link or the slug of the collection :)")
-                resp = await client.wait_for("message", timeout=120)
+                resp = await client.wait_for("message", timeout=TIMEOUT)
                 slug, name, status = get_opensea_slug(resp)
                 dict_data = get_dict_data()
                 if 'Error' not in status:
                     msg = await message.channel.send("Only get an alert if floor falls below a threshold.\n"
                                                      "Set your threshold (e.g. 0.2 or 0 if you want to get an alert for every floor change):")
-                    resp = await client.wait_for("message", timeout=120)
+                    resp = await client.wait_for("message", timeout=TIMEOUT)
                     args = resp.content.strip().split(" ")
                     if len(args) == 1:
                         user_input = args[0]
@@ -140,7 +177,7 @@ async def on_message(message: discord.Message):
             elif selected == "Royalty":
                 msg = await message.channel.send("For which collection do you want to get the royalties?\n"
                                                  "Please send me the opensea-link or the slug of the collection :)")
-                resp = await client.wait_for("message", timeout=120)
+                resp = await client.wait_for("message", timeout=TIMEOUT)
                 slug, name, status = get_opensea_slug(resp)
                 if "Error" not in status:
                     stats = getOScollection(slug)
@@ -155,13 +192,13 @@ async def on_message(message: discord.Message):
             elif selected == "Sniper":
                 msg = await message.channel.send("For which collection do you want to set up a sniper?\n"
                                                  "Please send me the opensea-link or the slug of the collection :)")
-                resp = await client.wait_for("message", timeout=120)
+                resp = await client.wait_for("message", timeout=TIMEOUT)
                 slug, name, status = get_opensea_slug(resp)
                 dict_data = get_dict_data()
                 if "Error" not in status:
                     msg = await message.channel.send("Get only listings under a threshold.\n"
                                                      "Set your threshold (eg. 0.2 or 0 if you dont want to set a threshold at all):")
-                    resp = await client.wait_for("message", timeout=120)
+                    resp = await client.wait_for("message", timeout=TIMEOUT)
                     args = resp.content.strip().split(" ")
                     if len(args) == 1:
                         user_input = args[0]
@@ -181,13 +218,13 @@ async def on_message(message: discord.Message):
                                 SelectOption("Yes", label="Yes", description="Add Trait Filter"),
                                 SelectOption("No", label="No", description="No Trait Filter")
                             ], max_values=1)])
-                            sel = await msg.wait_for("select", client, by=message.author, timeout=120)
+                            sel = await msg.wait_for("select", client, by=message.author, timeout=TIMEOUT)
                             add_filter = sel.selected_options[0].content
                             await sel.respond(f"You selected '{add_filter}'")
                             while add_filter == 'Yes':
                                 try:
                                     msg_trait = await message.channel.send("Select the trait:", components=[SelectMenu(options=selectTraits, max_values=1)])
-                                    sel = await msg_trait.wait_for("select", client, by=message.author, timeout=120)
+                                    sel = await msg_trait.wait_for("select", client, by=message.author, timeout=TIMEOUT)
                                     sel_trait = sel.selected_options[0].content
                                     if sel_trait not in dict_trait_filter:
                                         dict_trait_filter[sel_trait] = []
@@ -196,7 +233,7 @@ async def on_message(message: discord.Message):
                                     for trait in traits[sel_trait]:
                                         selectSubTraits.append(eval(f"SelectOption('{trait}', label='{trait}')"))
                                     msg_trait = await message.channel.send(f"Select the {sel_trait}-trait (multiple selection possible):", components=[SelectMenu(options=selectSubTraits, max_values=len(selectSubTraits))])
-                                    sel = await msg_trait.wait_for("select", client, by=message.author, timeout=120)
+                                    sel = await msg_trait.wait_for("select", client, by=message.author, timeout=TIMEOUT)
                                     for subtrait in sel.selected_options:
                                         if subtrait.content not in dict_trait_filter[sel_trait]:
                                             dict_trait_filter[sel_trait].append(subtrait.content)
@@ -206,7 +243,7 @@ async def on_message(message: discord.Message):
                                         # SelectOption("Edit", label="Edit", description="Edit Trait Filter"),
                                         SelectOption("No", label="No", description="No Trait Filter")
                                     ], max_values=1)])
-                                    sel = await msg.wait_for("select", client, by=message.author, timeout=120)
+                                    sel = await msg.wait_for("select", client, by=message.author, timeout=TIMEOUT)
                                     add_filter = sel.selected_options[0].content
                                     await sel.respond(f"You selected '{add_filter}'")
                                 except TimeoutError:
@@ -255,7 +292,7 @@ async def on_message(message: discord.Message):
             selectOptions.append(eval(f"SelectOption('{tool}{i}', label='{tool}: {collection}')"))
         msg = await message.channel.send("What do you want to stop?", components=[SelectMenu(options=selectOptions, max_values=1)])
         try:
-            sel = await msg.wait_for("select", client, by=message.author, timeout=120)
+            sel = await msg.wait_for("select", client, by=message.author, timeout=TIMEOUT)
             tool, collection = sel.selected_options[0].content.split(': ')
             del dict_data[tool][collection]['channel_id'][channel_id]
             if not dict_data[tool][collection]['channel_id']:
@@ -280,15 +317,17 @@ async def floor():
             dict_channel_id = collection['channel_id']
             slug            = collection['slug']
             stats           = getOSstats(slug)
-            dict_floor      = get_last_floor(collection['name'])
-            last_floor      = dict_floor[collection['name']]
+            # dict_floor      = get_last_floor(collection['name'])
+            last_floor      = collection['last_floor']
+            # last_floor      = dict_floor[collection['name']]
 
             # Get floor price
             try:
                 floor_price = float(stats['floor_price'])
             except:
                 floor_price = 0
-            dict_floor[collection['name']] = floor_price
+            collection['last_floor'] = floor_price
+            dict_data['floor'][collection['name']] = collection
 
             # Trigger
             if abs(floor_price-last_floor) > 0:
@@ -336,7 +375,7 @@ async def floor():
                         await channel.send(embed=embed)
 
                 # Save collection
-                save_pickle(dict_floor, PICKLE_FILE_FLOOR)
+                save_pickle(dict_data, PICKLE_FILE_DATA)
         except:
             pass
 
@@ -345,6 +384,7 @@ async def floor():
 async def sniper():
     dict_data   = get_dict_data()
     dict_sniper = dict_data['sniper']
+    print(time.strftime('%X %x %Z'))
     print(yaml.dump(dict_data, sort_keys=False, default_flow_style=False))
     for collection_name in dict_sniper:
         try:
@@ -474,8 +514,57 @@ async def sniper():
 
                         # Store last_time
                         collection['last_listing'] = current_timestamp
-                        dict_sniper[collection_name] = collection
-                        save_pickle(dict_sniper, PICKLE_FILE_SNIPER)
+                        dict_data['sniper'][collection_name] = collection
+                        save_pickle(dict_data, PICKLE_FILE_DATA)
+        except:
+            pass
+
+
+@tasks.loop(seconds=60)
+async def coin():
+    dict_data = get_dict_data()
+    dict_coin = dict_data['coin']
+    for coin in dict_coin:
+        try:
+            coin = dict_coin[coin]
+            dict_channel_id = coin['channel_id']
+            slug = coin['slug']
+            last_price = coin['last_price']
+            coin_url = coin['url']
+            # coin_name = coin['name'],
+            # coin_currency = coin['currency']
+
+            name, currency, price = get_coin(slug)
+            for channel_id in dict_channel_id:
+                channel = client.get_channel(int(channel_id))
+                change_type, change_rate = dict_channel_id[channel_id]
+                if change_type == 'absolute':
+                    if abs(last_price - price) < change_rate:
+                        continue
+                elif change_type == 'relative':
+                    if abs(last_price / price - 1) < change_rate:
+                        continue
+                icon = ''
+                if last_price != 0:
+                    change_ratio = round((float(price/last_price)-1)*100, 2)
+                else:
+                    change_ratio = 0
+                change_ratio = str(change_ratio) + '%'
+                if price >= last_price:
+                    icon = ':rocket:'
+                    change_ratio = '+' + str(change_ratio)
+                elif price < last_price:
+                    icon = ':small_red_triangle_down:'
+                embed = discord.Embed(
+                    title=f"{icon} {price} EUR ({change_ratio}) - {name} Price",
+                    url=coin_url,
+                    color=discord.Color.blue()
+                )
+                embed.add_field(name=f"**{name}**", value=f"{price} EUR")
+                await channel.send(embed=embed)
+            coin['last_price'] = price
+            dict_data['coin'][currency] = coin
+            save_pickle(dict_data, PICKLE_FILE_DATA)
         except:
             pass
 
@@ -487,7 +576,7 @@ async def on_ready():
         print(f'{guild.name}(id: {guild.id})')
 
     floor.start()
-    # coin_tracking.start()
+    coin.start()
     sniper.start()
 
 
