6190661e2539c14c38cb65b08cf4d9a66065b898
Viet Anh <v1et4nh@googlemail.com>
2023-09-16 01:26:22 +0200

    [Wiesn_Alert] While Loop - Test

--- Playground/wiesn_alert.py
+++ Playground/wiesn_alert.py
@@ -1,4 +1,6 @@
 import os
+import time
+from time import sleep
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.firefox import GeckoDriverManager
@@ -6,32 +8,45 @@ from selenium.webdriver.common.by import By
 from Functions.telegrambot import telegram_bot_sendtext
 
 
-URL = "https://www.oktoberfest-booking.com/de/reseller-angebote"
-os.environ['MOZ_HEADLESS'] = '1'
-if os.name == 'nt':
-    driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
-else:
-    driver = webdriver.Firefox()
-driver.get(URL)
-gesamt = driver.find_element(By.CSS_SELECTOR, "div.tw-mt-2:nth-child(2)")
-data   = gesamt.text.split('\n')
+def main():
+    URL = "https://www.oktoberfest-booking.com/de/reseller-angebote"
+    os.environ['MOZ_HEADLESS'] = '1'
+    if os.name == 'nt':
+        driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+    else:
+        driver = webdriver.Firefox()
+    driver.get(URL)
+    gesamt = driver.find_element(By.CSS_SELECTOR, "div.tw-mt-2:nth-child(2)")
+    data   = gesamt.text.split('\n')
 
-dict_data = {'vormittag': [],
-             'mittag':    [],
-             'abend':     []}
-if 'Reservierungen für Tische am Abend' in data:
-    idx = data.index('Reservierungen für Tische am Abend')
-    dict_data['abend'] = data[idx+1:]
-    data  = data[:idx]
-if 'Reservierungen für Tische am Mittag' in data:
-    idx = data.index('Reservierungen für Tische am Mittag')
-    dict_data['mittag'] = data[idx+1:]
-    data   = data[:idx]
-if 'Reservierungen für Tische am Vormittag' in data:
-    dict_data['vormittag'] = data[1:]
+    dict_data = {'vormittag': [],
+                 'mittag':    [],
+                 'abend':     []}
+    if 'Reservierungen für Tische am Abend' in data:
+        idx = data.index('Reservierungen für Tische am Abend')
+        dict_data['abend'] = data[idx+1:]
+        data  = data[:idx]
+    if 'Reservierungen für Tische am Mittag' in data:
+        idx = data.index('Reservierungen für Tische am Mittag')
+        dict_data['mittag'] = data[idx+1:]
+        data   = data[:idx]
+    if 'Reservierungen für Tische am Vormittag' in data:
+        dict_data['vormittag'] = data[1:]
 
-if dict_data['abend']:
-    message = dict_data['abend']
-    telegram_bot_sendtext(message, bot_chatID='-1001575230467', disable_web_page_preview=True)
+    if dict_data['vormittag']:
+        message = dict_data['vormittag']
+        telegram_bot_sendtext(message, bot_chatID='-1001575230467', disable_web_page_preview=True)
 
-driver.close()
+    driver.close()
+    print(f"Success: {dict_data['vormittag']}")
+
+
+if __name__ == '__main__':
+    while True:
+        try:
+            print(time.strftime('%X %x %Z'))
+            main()
+            sleep(300)
+        except:
+            print('Restart...')
+            sleep(300)
